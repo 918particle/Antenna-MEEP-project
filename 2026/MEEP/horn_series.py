Solved the normalization bug, frequency sweep complete.

2026/MEEP/horn_series.py
@@ -9,14 +9,14 @@ import radiation_pattern
 import graph_patterns
 import constants as c
 mp.verbosity(0)
-
-#Create the simulation
-pml_layers = [mp.PML(1.0)]
-geometry = []
-sources = []
 frequencies = np.arange(c.sweep_start,c.sweep_stop,c.d_f)
+
 #Frequency sweep
 for f in frequencies:
+	#Create the simulation
+	pml_layers = [mp.PML(1.0)]
+	geometry = []
+	sources = []
 	RF_array(geometry,sources,f)
 	sim = mp.Simulation(
 		cell_size=mp.Vector3(c.x_size,c.y_size),
@@ -25,13 +25,13 @@ for f in frequencies:
 		sources=sources,
 		resolution=c.resolution)
 	#Add Near 2 Far projection regions
-	region = Near2Far_Region(c.n2f_x_size,c.n2f_y_size,np.array([c.base_frequency]))
+	region = Near2Far_Region(c.n2f_x_size,c.n2f_y_size,np.array([c.base_frequency,f]))
 	region_objs = region.create(sim)
 	#Run statement
 	sim.run(until=100)
 	#Compute radiation patterns
+	theory_results = radiation_pattern.calculate_theoretical_radiation_pattern(0.0,f)
 	results = radiation_pattern.calculate_radiation_pattern(sim,region_objs)
-	#theory_results = radiation_pattern.calculate_theoretical_radiation_pattern(0.0,c.base_frequency)
 	#angles=((results[0][0]+np.pi)%(2*np.pi)-np.pi)*180.0/np.pi
 	#for i in range(c.npts):
 	#	print(angles[i],end=' ')
@@ -43,9 +43,10 @@ for f in frequencies:
 	#beams = radiation_pattern.locate_beams(results)
 	#print(beams)
 	#Graph radiation patterns
-	graph_patterns.plot_radiation_patterns(results,"rad_pattern"+"_"+format(f,".2f")+"_.png")
+	graph_patterns.plot_radiation_patterns(results,"rad_pattern"+"_"+format(f,".2f")+".png")
+	#Plot surfaces
+	if(f==frequencies[0]):
+		fg = plt.figure(dpi=150)
+		sim.plot2D(ax=fg.gca(),eps_parameters={'cmap': 'gray'})
+		plt.savefig("surfaces.png")
 	sim.reset_meep()
-#Plot surfaces
-fg = plt.figure(dpi=150)
-sim.plot2D(ax=fg.gca(),eps_parameters={'cmap': 'gray'})
-plt.savefig("surfaces.png")

2026/MEEP/radiation_pattern.py
@@ -17,7 +17,7 @@ def calculate_radiation_pattern(simulation,n2f_objs):
 		Py = np.real(E[:,2]*H[:,0]-E[:,0]*H[:,2])
 		Pz = np.real(E[:,0]*H[:,1]-E[:,1]*H[:,0])
 		Pr = np.sqrt(np.square(Px)+np.square(Py)+np.square(Pz))
-		directivity = 10.0*np.log10(Pr/constants.normalization)
+		directivity = 10.0*np.log10(Pr)-10.0*np.log10(constants.normalization)
 		results.append((angles,directivity,"data"))
 	return results
 def calculate_theoretical_radiation_pattern(phi_0,frequency):
@@ -30,7 +30,7 @@ def calculate_theoretical_radiation_pattern(phi_0,frequency):
 			pr_th_num = np.sin(constants.n_antenna*np.pi*constants.d_y*frequency*(np.sin(theta)-np.sin(phi_0)))
 			pr_th_den = np.sin(1.0*np.pi*constants.d_y*frequency*(np.sin(theta)-np.sin(phi_0)))
 			directivity.append(pr_th_num*pr_th_num/pr_th_den/pr_th_den/constants.n_antenna/constants.n_antenna)
-	directivity = 10.0*np.log10(directivity)-10.0*np.log10(constants.theory_normalization)
+	directivity = 10.0*np.log10(directivity)
 	return [(angles,directivity,"theory")]
 def locate_beams(rad_patterns):
 	results = []