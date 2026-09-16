b8d28a647e447b6c77416d79b291e27a9044e5be
Jordan C hanson <918particle@gmail.com>
2026-09-16 14:49:53 -0700

    running radiation patterns to match lab data at all freqs

--- horn_w_cable.py
+++ horn_w_cable.py
@@ -5,7 +5,7 @@ def main():
     resolution = 10
     sigma = 0.25
     mu = 1
-    cw_frequency = 0.3
+    cw_frequency = 0.5
     rad_or_vswr = 1
     e_or_h_plane = 1
     Plan(resolution,cw_frequency,sigma,mu,rad_or_vswr,e_or_h_plane)
